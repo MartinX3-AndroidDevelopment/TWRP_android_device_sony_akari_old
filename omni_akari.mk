1b21aad558601954c529c840ee4693278ca47d0d
Martin Duenkelmann <nc-duenkekl3@netcologne.de>
2018-08-01 19:36:43 +0200

    build the kernel with twrp fixed the path to the timezone data

--- omni_akari.mk
+++ omni_akari.mk
@@ -57,7 +57,7 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 
 # Time Zone data for recovery
 PRODUCT_COPY_FILES += \
-    system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_akari
